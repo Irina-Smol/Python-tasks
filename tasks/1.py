b4da3e694a2c4c76658c000bb83b8f5231c4ab7b
Ирина <Smolenskaya2002@yandex.ru>
2023-02-14 11:17:24 +0300

    Create 1.py

---
+++ tasks/1.py
@@ -0,0 +1,16 @@
+the_dict = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
+}
+
+a = int(input())
+if a in the_dict:
+   print(the_dict[a])
